03dac05c6ec7f96cd64fe8d1efc35b5eb7052dc2
Annise Arif <83833939+annise-arif@users.noreply.github.com>
2023-07-26 23:47:18 +0600

    adding product details slider left right arrow

--- src/API/JustForYouDataById.tsx
+++ src/API/JustForYouDataById.tsx
@@ -1,7 +1,17 @@
 import {useState, useEffect} from 'react';
 
+export interface Service {
+    _id: number
+    img: string
+    title: string
+    new_price: number
+    old_price: number
+    purcent: string
+    stars: number
+    reviews: number
+  }
 const JustForYouDataById = (_id: String | undefined) => {
-    const [service, setService] = useState([]);
+    const [service, setService] = useState<Service[]>([]);
     useEffect(() => {
         fetch(`https://daraz-server.vercel.app/services/${_id}`)
         .then(res => res.json())
